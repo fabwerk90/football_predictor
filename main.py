9540f1f736c0cac079754a92bd60e6f8f42e7184
Fabian Werkmeister <fabwerk90@gmail.com>
2025-02-06 14:05:42 +0100

    Update matchday to 21 and adjust fixture results in CSV

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from matchpredictor import MatchPredictor
 from teamperformance import TeamPerformance
 from weightscalculator import WeightsCalculator
 
-MATCHDAY = 14
+MATCHDAY = 21
 
 # Initialize the class with the current season
 bundesliga_data = GetBundesligaData(CURRENT_SEASON)
